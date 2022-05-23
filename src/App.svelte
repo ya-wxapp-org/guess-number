96c9f2688e5e3f5a68d420b11540e14b0323a050
fimars <foru.fy@gmail.com>
2022-05-23 14:48:01 +0800

    cool guess

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+  let random = Math.floor(Math.random() * 100);
+  let guess = 50;
+  let result = "TRY ?!";
+
+  function handleSubmit(event) {
+    console.log(random);
+    result = guess === random 
+      ? 'Bingo!' 
+      : guess < random
+        ? 'TRY BIGGER'
+        : 'TRY LITTER'  
+  }
 </script>
 
 <main>
   <p>A Template with swc</p>
+
+  <h2>{result}</h2>
+  
+  <input bind:value={guess} type="number" placeholder="enter your guess">
+  <button on:click={handleSubmit}>submit</button>
 </main>
 
 <style>
